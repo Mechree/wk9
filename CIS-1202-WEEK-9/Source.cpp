028270a381a892ee388dde18808e072e91382ead
Michael McCain <michaelmc1989@gmail.com>
2022-03-15 13:13:24 -0400

    Created the inital program to test the procedures for updating an application through GitHub.

---
+++ CIS-1202-WEEK-9/Source.cpp
@@ -0,0 +1,17 @@
+// Michael McCain
+// 3/15/2022
+// CIS-1202
+// GitHub Test Program
+// This program will test the process of updating an application through GitHub
+
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Testing 1....2....3....";
+	// System Exit
+	cout << endl << endl;
+	return 0;
+}
